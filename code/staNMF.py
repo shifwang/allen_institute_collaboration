cefff6ace10c00c5495d5fb09f5c76085982c882
EC2 Default User <ec2-user@ip-172-16-53-241.us-west-2.compute.internal>
2019-03-01 08:23:17 +0000

    add staNMF and its unit test

--- code/staNMF.py
+++ code/staNMF.py
@@ -1,5 +1,5 @@
 import numpy as np
-import sklearn 
+import sklearn.preprocessing
 import time
 import pickle
 import os, sys
@@ -13,8 +13,7 @@ class instability:
         self.n_trials = n_trials
     def fit_transform(self, Ks):
         self.fit(Ks)
-        output = self.transform(Ks)
-        self.ins_mean, self.ins_SE = output[:,0], output[:,1]
+        return self.transform(Ks)
     def fit(self, Ks):
         for k in Ks:
             for i in range(self.n_trials):
@@ -30,9 +29,83 @@ class instability:
                     os.makedirs(os.path.dirname(filename))
                 with open(filename, 'wb') as f:
                     pickle.dump(nmf, f)
+    def amariMaxError_(self, correlation):
+        '''
+        Computes what Wu et al. (2016) described as a 'amari-type error'
+        based on average distance between factorization solutions
+
+        Return:
+        Amari distance distM
+
+        Arguments:
+        :param: correlation: k by k matrix of pearson correlations
+
+        Usage: Called by self.transform(Ks)
+        '''
+
+        n, m = correlation.shape
+        assert n == m, 'correlation matrix must be square.'
+        maxCol = np.absolute(correlation).max(0)
+        colTemp = np.mean((1-maxCol))
+        maxRow = np.absolute(correlation).max(1)
+        rowTemp = np.mean((1-maxRow))
+        distM = (rowTemp + colTemp)/(2)
+
+        return distM
+    def findcorrelation_(self, A, B):
+        '''
+        Construct k by k matrix of Pearson product-moment correlation
+        coefficients for every combination of two columns in A and B
+
+        :param: A : first NMF solution matrix
+        :param: B : second NMF solution matrix, of same dimensions as A
+
+        Return: numpy array of dimensions k by k, where array[a][b] is the
+        correlation between column 'a' of X and column 'b'
+
+        Usage:
+        Called by self.transform(Ks)
+
+        '''
+        #corrmatrix = []
+        #for a in range(k):
+        #    for b in range(k):
+        #        c = np.corrcoef(A[:, a], B[:, b])
+        #        corrmatrix.append(c[0][1])
+        #return np.asarray(corrmatrix).reshape(k, k)
+        A_std = sklearn.preprocessing.scale(A)
+        B_std = sklearn.preprocessing.scale(B)
+        return A_std.T @ B_std / A.shape[0]
+
     def transform(self, Ks):
-        # TODO
-        pass
+        ins_mean, ins_SE = [], []
+        for k in Ks:
+            folder = self.folder_name + '/k=' + str(k)
+            if not os.path.exists(folder):
+                raise ValueError('folder %s not found, have you run fit first?'%folder)
+            Dhat = []
+            for filename in os.listdir(folder):
+                with open(os.path.join(folder, filename), 'rb') as f:
+                    nmf = pickle.load(f)
+                Dhat.append(nmf.components_.T)
+            num = len(Dhat)
+            if num == 0:
+                raise ValueError('folder %s is empty, have you run fit first?'%folder)
+            distMat = np.zeros(shape=(num, num))
+            for i in range(num):
+                for j in range(i, num):
+                    x = Dhat[i]
+                    y = Dhat[j]
+                    CORR = self.findcorrelation_(x, y)
+                    distMat[i][j] = self.amariMaxError_(CORR)
+                    distMat[j][i] = distMat[i][j]
+            ins_mean.append(np.sum(distMat) / (num *(num-1)))
+            ins_SE.append((np.sum(distMat**2) / (num*(num - 1)) - ins_mean[-1]**2)**.5 * (2 / distMat.shape[0])**.5)
+        output = np.array([ins_mean, ins_SE]).T
+        return output
+                        
+
+                
         
                     
             
