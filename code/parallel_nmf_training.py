53cca6ace30c48c74dfb054ffeefd261bf5ef32b
yu wang <wang.yu@berkeley.edu>
2019-11-14 17:54:32 +0000

    add parallel processing py

---
+++ code/parallel_nmf_training.py
@@ -0,0 +1,17 @@
+import numpy as np
+import sklearn
+from nmf_with_missing_values import nmf_with_missing_values
+from multiprocessing import Pool
+def f(i):
+    tmp = np.load('../data/mouse_brain_ISH_float32.npz')
+    data = tmp['data']
+    sections = tmp['sections']
+    original_shape = data.shape
+    d = data.shape[1] * data.shape[2] * data.shape[3]
+    data = np.reshape(data, (data.shape[0], d))
+    nmf = nmf_with_missing_values(n_outer_loops = 4, n_components = 18, init = 'nndsvd', random_state = None)
+    D = nmf.fit_transform(sklearn.util.resample(data, random_state = i))
+    A = nmf.components_
+    np.savez('stability_result_comp_18_{}.npz'.format(i), D = D, A = A, X_guess = None, data = None, original_shape = original_shape)
+a = Pool(20)
+a.map(f, range(20))
